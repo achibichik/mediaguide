cf79011f718f2d5e0e37b52b62df0e43e56fc4e4
achibichik <chibichikalena@gmail.com>
2024-12-25 05:39:07 +0500

    commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Добавление плавного скроллинга для якорных ссылок
+document.querySelectorAll('.navigation a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({ behavior: 'smooth' });
+    });
+});
